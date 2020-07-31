63468f479e94ad116eee82c6dfee0a02e468a268
Robert Waffen <rwaffen@gmail.com>
2020-08-01 01:56:18 +0200

    update sub_folders on changes

--- lib/Helpers.rb
+++ lib/Helpers.rb
@@ -53,10 +53,13 @@ def write_folders_to_db(folder_hash)
       folder.sub_folders   = folder_path[:sub_folders]
       folder.md5_path      = folder_path[:md5_path]
     end
-    Folder.update_all(
-      {sub_folders: folder_path[:sub_folders]},
-      {md5_path: folder_path[:md5_path]}
-    )
+
+    updates = Folder.find_by(md5_path: folder_path[:md5_path])
+    if updates.sub_folders != folder_path[:sub_folders]
+       updates.sub_folders = folder_path[:sub_folders]
+       updates.save
+    end
+
   end
 end
 
