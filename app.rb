ec5d5806d40491ac2d439d00934ed908caec2461
Robert Waffen <rwaffen@gmail.com>
2021-01-29 18:02:45 +0100

    chnage application to modular style

--- app.rb
+++ app.rb
@@ -7,7 +7,7 @@ require 'mini_magick'
 require 'octicons'
 require 'phashion'
 require 'securerandom'
-require 'sinatra'
+require 'sinatra/base'
 require 'sinatra/activerecord'
 require 'sinatra/custom_logger'
 require 'streamio-ffmpeg'
@@ -19,220 +19,224 @@ require_relative 'lib/BootstrapLinkRenderer'
 require_relative 'lib/Helpers'
 require_relative 'lib/Models'
 
-include ActionView::Helpers::TextHelper
-Config.load_and_set_settings "#{File.dirname(__FILE__)}/config/settings.yml"
+class Kohinga < Sinatra::Base
+  include ActionView::Helpers::TextHelper
+  register Sinatra::ActiveRecordExtension
 
-set :method_override, true
-set :logger, Logger.new($stdout)
-set :session_secret, SecureRandom.uuid
-set :database, {
-  adapter: 'sqlite3',
-  database: Settings.db_path,
-  pool: 5,
-  timeout: 5000,
-  options: Settings.db_options
-}
+  Config.load_and_set_settings "#{File.dirname(__FILE__)}/config/settings.yml"
 
-enable :sessions
+  set :method_override, true
+  set :logger, Logger.new($stdout)
+  set :session_secret, SecureRandom.uuid
+  set :database, {
+    adapter: 'sqlite3',
+    database: Settings.db_path,
+    pool: 5,
+    timeout: 5000,
+    options: Settings.db_options
+  }
 
-get '/' do
-  erb :index, locals: { message: nil }
-end
+  enable :sessions
 
-get '/config' do
-  erb :config
-end
+  get '/' do
+    erb :index, locals: { message: nil }
+  end
 
-get '/duplicates' do
-  erb :duplicates
-end
+  get '/config' do
+    erb :config
+  end
 
-get '/duplicate/scan' do
-  find_duplicates
-  erb :index, locals: { message: 'Duplicate Scan ready' }
-end
+  get '/duplicates' do
+    erb :duplicates
+  end
 
-get '/favorites' do
-  erb :favorites
-end
+  get '/duplicate/scan' do
+    find_duplicates
+    erb :index, locals: { message: 'Duplicate Scan ready' }
+  end
 
-post '/favorite/:md5' do
-  image = Image.find_by(md5_path: params[:md5])
-  image.update_attribute(:favorite, params[:favorite])
-end
+  get '/favorites' do
+    erb :favorites
+  end
 
-get '/folders' do
-  erb :folders, locals: { folder_root: "#{Settings.originals_path}/" }
-end
+  post '/favorite/:md5' do
+    image = Image.find_by(md5_path: params[:md5])
+    image.update_attribute(:favorite, params[:favorite])
+  end
 
-get '/folders/*' do |path|
-  erb :folders, locals: { folder_root: path }
-end
+  get '/folders' do
+    erb :folders, locals: { folder_root: "#{Settings.originals_path}/" }
+  end
 
-post '/folder/create' do
-  if params[:add_folder]
-    folder_path   = params['add_folder']
-    folder_path   = "#{params['add_folder']}/" if folder_path[-1, 1] != '/'
+  get '/folders/*' do |path|
+    erb :folders, locals: { folder_root: path }
+  end
 
-    md5_path      = Digest::MD5.hexdigest(folder_path)
+  post '/folder/create' do
+    if params[:add_folder]
+      folder_path   = params['add_folder']
+      folder_path   = "#{params['add_folder']}/" if folder_path[-1, 1] != '/'
 
-    # cut slash  from folder_path to get parent and than add slash to parent,
-    # because all pathes end with a slash
-    parent_folder = "#{File.dirname(folder_path.delete_suffix('/'))}/"
-    parent_md5    = Digest::MD5.hexdigest(parent_folder)
+      md5_path      = Digest::MD5.hexdigest(folder_path)
 
-    FileUtils.mkdir_p folder_path
-    puts "####### created folder: #{folder_path}"
+      # cut slash  from folder_path to get parent and than add slash to parent,
+      # because all pathes end with a slash
+      parent_folder = "#{File.dirname(folder_path.delete_suffix('/'))}/"
+      parent_md5    = Digest::MD5.hexdigest(parent_folder)
 
-    Folder.find_or_create_by(md5_path: md5_path) do |folder|
-      folder.folder_path   = folder_path
-      folder.parent_folder = parent_folder
-      folder.sub_folders   = Dir.glob("#{folder_path}*/")
-      folder.md5_path      = md5_path
+      FileUtils.mkdir_p folder_path
+      puts "####### created folder: #{folder_path}"
+
+      Folder.find_or_create_by(md5_path: md5_path) do |folder|
+        folder.folder_path   = folder_path
+        folder.parent_folder = parent_folder
+        folder.sub_folders   = Dir.glob("#{folder_path}*/")
+        folder.md5_path      = md5_path
+      end
+
+      updates = Folder.find_by(md5_path: parent_md5)
+      if updates.sub_folders != Dir.glob("#{parent_folder}*/")
+        updates.sub_folders = Dir.glob("#{parent_folder}*/")
+        updates.save
+      end
     end
 
-    updates = Folder.find_by(md5_path: parent_md5)
+    redirect back
+  end
+
+  delete '/folder/delete/:md5' do
+    folder        = Folder.find_by(md5_path: params[:md5])
+    parent_folder = folder.parent_folder.to_s
+
+    FileUtils.rm_r folder.folder_path if File.directory?(folder.folder_path)
+    puts "####### delete folder: #{folder.folder_path}"
+
+    updates = Folder.find_by(folder_path: parent_folder)
     if updates.sub_folders != Dir.glob("#{parent_folder}*/")
       updates.sub_folders = Dir.glob("#{parent_folder}*/")
       updates.save
     end
+
+    folder.destroy
+    redirect "/folders/#{parent_folder}"
   end
 
-  redirect back
-end
+  post '/folder/move/:md5' do
+    folder            = Folder.find_by(md5_path: params[:md5])
+    old_parent_folder = folder.parent_folder.to_s
 
-delete '/folder/delete/:md5' do
-  folder        = Folder.find_by(md5_path: params[:md5])
-  parent_folder = folder.parent_folder.to_s
+    new_folder_path   = params['move_folder']
+    new_md5_path      = Digest::MD5.hexdigest(new_folder_path)
+    new_parent_folder = "#{File.dirname(new_folder_path.delete_suffix('/'))}/"
+    new_parent_md5    = Digest::MD5.hexdigest(new_parent_folder)
 
-  FileUtils.rm_r folder.folder_path if File.directory?(folder.folder_path)
-  puts "####### delete folder: #{folder.folder_path}"
+    FileUtils.mv folder.folder_path, new_folder_path
+    puts "####### moved folder: from #{folder.folder_path} to #{new_folder_path}"
 
-  updates = Folder.find_by(folder_path: parent_folder)
-  if updates.sub_folders != Dir.glob("#{parent_folder}*/")
-    updates.sub_folders = Dir.glob("#{parent_folder}*/")
-    updates.save
-  end
-
-  folder.destroy
-  redirect "/folders/#{parent_folder}"
-end
-
-post '/folder/move/:md5' do
-  folder            = Folder.find_by(md5_path: params[:md5])
-  old_parent_folder = folder.parent_folder.to_s
-
-  new_folder_path   = params['move_folder']
-  new_md5_path      = Digest::MD5.hexdigest(new_folder_path)
-  new_parent_folder = "#{File.dirname(new_folder_path.delete_suffix('/'))}/"
-  new_parent_md5    = Digest::MD5.hexdigest(new_parent_folder)
-
-  FileUtils.mv folder.folder_path, new_folder_path
-  puts "####### moved folder: from #{folder.folder_path} to #{new_folder_path}"
-
-  Folder.find_or_create_by(md5_path: new_md5_path) do |folder|
-    folder.folder_path   = new_folder_path
-    folder.parent_folder = new_parent_folder
-    folder.sub_folders   = Dir.glob("#{new_folder_path}*/")
-    folder.md5_path      = new_md5_path
-  end
-
-  update_old_parent = Folder.find_by(folder_path: old_parent_folder)
-  if update_old_parent.sub_folders != Dir.glob("#{old_parent_folder}*/")
-    update_old_parent.sub_folders = Dir.glob("#{old_parent_folder}*/")
-    update_old_parent.save
-  end
-
-  update_new_parent = Folder.find_by(folder_path: new_parent_folder)
-  if update_new_parent.sub_folders != Dir.glob("#{new_parent_folder}*/")
-    update_new_parent.sub_folders = Dir.glob("#{new_parent_folder}*/")
-    update_new_parent.save
-  end
-
-  folder.destroy
-  redirect "/folders/#{new_folder_path}"
-end
-
-get '/image/:md5' do
-  image = Image.find_by(md5_path: params[:md5])
-  send_file(image.file_path.to_s, disposition: 'inline')
-end
-
-delete '/image/:md5' do
-  image = Image.find_by(md5_path: params[:md5])
-  File.delete(image.file_path) if File.exist?(image.file_path)
-  image.destroy
-  redirect back
-end
-
-post '/image/upload' do
-  if params[:files]
-    folder_path = params[:file_target].delete_suffix('/')
-
-    params['files'].each do |file|
-      target   = "#{folder_path}/#{file[:filename]}"
-      md5_path = Digest::MD5.hexdigest(target)
-
-      File.open(target, 'wb') { |f| f.write file[:tempfile].read }
-
-      Image.find_or_create_by(md5_path: md5_path) do |image|
-        is_video = true if Settings.movie_extentions.include? File.extname(file[:filename]).delete('.')
-
-        is_image = true if Settings.image_extentions.include? File.extname(file[:filename]).delete('.')
-
-        image.file_path   = target
-        image.folder_path = folder_path
-        image.image_name  = File.basename(file[:filename], '.*')
-        image.md5_path    = md5_path
-        image.is_image    = is_image
-        image.is_video    = is_video
-      end
-
-      create_thumb(md5_path, Settings.thumb_target, Settings.thumb_res)
+    Folder.find_or_create_by(md5_path: new_md5_path) do |folder|
+      folder.folder_path   = new_folder_path
+      folder.parent_folder = new_parent_folder
+      folder.sub_folders   = Dir.glob("#{new_folder_path}*/")
+      folder.md5_path      = new_md5_path
     end
+
+    update_old_parent = Folder.find_by(folder_path: old_parent_folder)
+    if update_old_parent.sub_folders != Dir.glob("#{old_parent_folder}*/")
+      update_old_parent.sub_folders = Dir.glob("#{old_parent_folder}*/")
+      update_old_parent.save
+    end
+
+    update_new_parent = Folder.find_by(folder_path: new_parent_folder)
+    if update_new_parent.sub_folders != Dir.glob("#{new_parent_folder}*/")
+      update_new_parent.sub_folders = Dir.glob("#{new_parent_folder}*/")
+      update_new_parent.save
+    end
+
+    folder.destroy
+    redirect "/folders/#{new_folder_path}"
   end
 
-  redirect back
-end
-
-post '/image/move/:md5' do
-  new_file_path = params[:file_path]
-  image         = Image.find_by(md5_path: params[:md5])
-  new_md5_path  = Digest::MD5.hexdigest(new_file_path)
-
-  FileUtils.mv image.file_path, new_file_path
-  puts "####### moved image: from #{image.file_path} to #{new_file_path}"
-
-  Image.find_or_create_by(md5_path: new_md5_path) do |image|
-    is_video = true if Settings.movie_extentions.include? File.extname(new_file_path).delete('.')
-
-    is_image = true if Settings.image_extentions.include? File.extname(new_file_path).delete('.')
-
-    image.file_path   = new_file_path
-    image.folder_path = File.dirname(new_file_path)
-    image.image_name  = File.basename(new_file_path, '.*')
-    image.md5_path    = new_md5_path
-    image.is_image    = is_image
-    image.is_video    = is_video
+  get '/image/:md5' do
+    image = Image.find_by(md5_path: params[:md5])
+    send_file(image.file_path.to_s, disposition: 'inline')
   end
 
-  create_thumb(new_md5_path, Settings.thumb_target, Settings.thumb_res)
+  delete '/image/:md5' do
+    image = Image.find_by(md5_path: params[:md5])
+    File.delete(image.file_path) if File.exist?(image.file_path)
+    image.destroy
+    redirect back
+  end
 
-  image.destroy
-  redirect back
-end
+  post '/image/upload' do
+    if params[:files]
+      folder_path = params[:file_target].delete_suffix('/')
 
-get '/indexer' do
-  build_index(
-    Settings.originals_path,
-    Settings.thumb_target,
-    Settings.thumb_res,
-    Settings.image_extentions + Settings.movie_extentions
-  )
-  erb :index, locals: { message: 'Index ready' }
-end
+      params['files'].each do |file|
+        target   = "#{folder_path}/#{file[:filename]}"
+        md5_path = Digest::MD5.hexdigest(target)
 
-get '/testing' do
-  duplicates = Image.find_by(fingerprint: '13153662325975432931')
-  puts duplicates.file_path
+        File.open(target, 'wb') { |f| f.write file[:tempfile].read }
+
+        Image.find_or_create_by(md5_path: md5_path) do |image|
+          is_video = true if Settings.movie_extentions.include? File.extname(file[:filename]).delete('.')
+
+          is_image = true if Settings.image_extentions.include? File.extname(file[:filename]).delete('.')
+
+          image.file_path   = target
+          image.folder_path = folder_path
+          image.image_name  = File.basename(file[:filename], '.*')
+          image.md5_path    = md5_path
+          image.is_image    = is_image
+          image.is_video    = is_video
+        end
+
+        create_thumb(md5_path, Settings.thumb_target, Settings.thumb_res)
+      end
+    end
+
+    redirect back
+  end
+
+  post '/image/move/:md5' do
+    new_file_path = params[:file_path]
+    image         = Image.find_by(md5_path: params[:md5])
+    new_md5_path  = Digest::MD5.hexdigest(new_file_path)
+
+    FileUtils.mv image.file_path, new_file_path
+    puts "####### moved image: from #{image.file_path} to #{new_file_path}"
+
+    Image.find_or_create_by(md5_path: new_md5_path) do |image|
+      is_video = true if Settings.movie_extentions.include? File.extname(new_file_path).delete('.')
+
+      is_image = true if Settings.image_extentions.include? File.extname(new_file_path).delete('.')
+
+      image.file_path   = new_file_path
+      image.folder_path = File.dirname(new_file_path)
+      image.image_name  = File.basename(new_file_path, '.*')
+      image.md5_path    = new_md5_path
+      image.is_image    = is_image
+      image.is_video    = is_video
+    end
+
+    create_thumb(new_md5_path, Settings.thumb_target, Settings.thumb_res)
+
+    image.destroy
+    redirect back
+  end
+
+  get '/indexer' do
+    build_index(
+      Settings.originals_path,
+      Settings.thumb_target,
+      Settings.thumb_res,
+      Settings.image_extentions + Settings.movie_extentions
+    )
+    erb :index, locals: { message: 'Index ready' }
+  end
+
+  get '/testing' do
+    duplicates = Image.find_by(fingerprint: '13153662325975432931')
+    puts duplicates.file_path
+  end
 end
