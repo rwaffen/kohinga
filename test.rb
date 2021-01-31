09ef24bad3b2d341df7460207d5aee2dbe48b2ef
Robert Waffen <rwaffen@gmail.com>
2021-01-31 22:35:00 +0100

    update test rb

--- test.rb
+++ test.rb
@@ -1,19 +1,42 @@
 require 'config'
 require 'digest'
 require 'fileutils'
+require 'find'
+require 'logger'
 require 'mini_magick'
+require 'pp'
 require 'sinatra/activerecord'
 require 'streamio-ffmpeg'
 require 'yaml'
 
-require_relative 'lib/Helpers'
-require_relative 'lib/Models'
+require_relative 'lib/helpers'
+require_relative 'lib/models'
 
 Config.load_and_set_settings("#{File.dirname(__FILE__)}/config/settings.yml")
 
-build_index(
-  Settings.originals_path,
-  Settings.thumb_target,
-  Settings.thumb_res,
-  Settings.image_extentions + Settings.movie_extentions
+def logger
+  @logger ||= Logger.new($stdout)
+end
+
+ActiveRecord::Base.establish_connection(
+  adapter: 'sqlite3',
+  database: Settings.db_path,
+  pool: 5,
+  timeout: 5000,
+  options: Settings.db_options
 )
+
+time         = Time.now
+image_root   = Settings.originals_path
+extensions   = Settings.image_extentions + Settings.movie_extentions
+thumb_target = Settings.thumb_target
+thumb_size   = Settings.thumb_res
+
+remove_file(thumb_target)
+remove_folder
+write_folders_to_db(index_folders(image_root))
+index_files_to_db(image_root, extensions)
+create_thumbs(thumb_target, thumb_size)
+# find_duplicates
+
+puts "search took #{Time.now - time}sec"
